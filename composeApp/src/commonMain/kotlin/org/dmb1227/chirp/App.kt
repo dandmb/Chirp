43cc2aa1390cbcc7d3e3d9a09c4f3652be3c014b
dandmb <danbizwa@gmail.com>
2026-06-23 16:42:50 +0200

    module relation done

--- composeApp/src/commonMain/kotlin/org/dmb1227/chirp/App.kt
+++ composeApp/src/commonMain/kotlin/org/dmb1227/chirp/App.kt
@@ -1,10 +1,12 @@
 package org.dmb1227.chirp
 
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
@@ -13,6 +15,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 
@@ -24,7 +27,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         val layoutDirection = LocalLayoutDirection.current
-        Surface (
+        Surface(
             modifier = Modifier
                 .padding(
                     start = WindowInsets.safeDrawing.asPaddingValues()
@@ -32,8 +35,10 @@ fun App() {
                     end = WindowInsets.safeDrawing.asPaddingValues()
                         .calculateEndPadding(layoutDirection)
                 ),
-        ){
-            Text("Hello there !!!")
+        ) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("Hello there !!!")
+            }
         }
 
     }
--- feature/chat/presentation/build.gradle.kts
+++ feature/chat/presentation/build.gradle.kts
@@ -60,29 +60,22 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
+                implementation(projects.core.domain)
+                implementation(projects.core.designsystem)
+                implementation(projects.feature.chat.domain)
+
+
+
                 // Add KMP dependencies here
             }
         }
 
-        commonTest {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
 
         androidMain {
             dependencies {
                 // Add Android-specific dependencies here. Note that this source set depends on
                 // commonMain by default and will correctly pull the Android artifacts of any KMP
                 // dependencies declared in commonMain.
-            }
-        }
-
-        getByName("androidDeviceTest") {
-            dependencies {
-                implementation(libs.androidx.junit)
-                implementation(libs.androidx.runner)
-                implementation(libs.androidx.test.core)
             }
         }
 
